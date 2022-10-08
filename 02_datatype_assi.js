9d48112cc653c51fbca86dee1552237b0f2415a1
Shubham15112000 <shubhamsawale117@gmail.com>
2022-10-08 08:22:23 +0530

    funass
    funass

--- 02_datatype_assi.js
+++ 02_datatype_assi.js
@@ -1,13 +1,15 @@
+
+console.log("Question:1");
 var pin = 431705;
 var city = "hingoli";
 var married_status = false;
-
 console.log( pin, typeof pin );
 console.log(city, typeof city);
 console.log(married_status, typeof married_status );
+console.log("------------------------");
 
+console.log("Question : 2");
 var forUndefine=
 console.log(forUndefine);
-
 var forNull = null;
 console.log( null);
--- 03_function_assign.js
+++ 03_function_assign.js
@@ -1,4 +1,35 @@
 
-function name(){
-    
+function shubham(){
+    console.log("Thise are my information");
 }
+shubham()
+
+function sawale(){
+    console.log("Shubham Vishwanath Sawale");
+}
+sawale()
+console.log("-------------Q-2----------");
+function rudra( arg1, arg2){
+    console.log( arg1,arg2);    
+}
+rudra( "Shubham", "Sawale");
+
+console.log("---------Q-3------------");
+function rudra( arg1,arg2){
+    console.log("---------- Before swap-------------");
+    console.log(arg1, arg2);
+    console.log("---------- After swap-------------");
+    var temp = arg1;
+    arg1 = arg2;
+    arg2 = temp;
+    console.log(arg1, arg2);
+}
+rudra("Virat", "Anushka");
+rudra("1000","2000")
+
+console.log("--------Q-4---------");
+function addition( arg1,arg2,arg3){
+    console.log( arg1 + arg2 + arg3);
+}
+addition(10.23,600,40);
+addition("Hello","Good","Morning");
